a8e2936d54054b3e41e8b0ecad39b2b87ff2ba2a
lau_py@yahoo.com <motorola1>
2019-09-16 18:47:23 -0700

    update read_excel function show plot usin matplotlib

--- Analyser.py
+++ Analyser.py
@@ -1,11 +1,19 @@
 import pandas as pd
-
+import matplotlib as matplt
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+import numpy as np
 
 class Analyser:
     def readExcel(self):
-        file = "e:\\Worksheet in D  Lesson 2019 Applied Sriptong Using Python Python Elective (IT49450) - Project_14Mar19.xls"
+        rcParams.update({'figure.autolayout':True})
+        file = "e:\\Worksheet in D  Lesson 2019 Applied Sriptong Using Python Python Elective (IT49450) - Project_14Mar19.xlsx"
         data = pd.read_excel(file)
-        print(data)
+        data.columns.values[0]='date'
+        printdata = data.iloc[400:410,0:3]
+        print(printdata)
+        printdata.set_index('date').plot(kind='bar')
+        plt.show()
 
 analyser = Analyser()
 analyser.readExcel()
